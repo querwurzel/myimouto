3ad0eb3b0053a33525162589edd2e0010bf75aef
Renzo <asterixvader@gmail.com>
2016-09-13 14:27:35 -0500

    Removed metatags when post is forbidden #204

--- app/views/post/_social_meta.php
+++ app/views/post/_social_meta.php
@@ -3,8 +3,12 @@
 <?= $this->tag('meta', array('property' => 'og:title', 'content' => $this->html_title())) ?>
 <?= $this->tag('meta', array('property' => 'og:type', 'content' => 'article')) ?>
 <?= $this->tag('meta', array('property' => 'og:url', 'content' => $this->urlFor(array('post#show', 'id' => $this->post->id, 'only_path' => false)))) ?>
-<?= $this->tag('meta', array('property' => 'og:image', 'content' => $this->post->sample_url())) ?>
 <?= $this->tag('meta', array('property' => 'og:site_name', 'content' => CONFIG()->app_name)) ?>
 <?= $this->tag('meta', array('property' => 'og:description', 'content' => $this->post->tags())) ?>
 <?php # Reddit Thumbnail ?>
-<?= $this->tag('link', array('rel' => 'image_src', 'href' => $this->post->sample_url())) ?>
+<?php
+if (CONFIG()->can_see_post(current_user(), $this->post)) {
+    echo $this->tag('link', array('rel' => 'image_src', 'href' => $this->post->sample_url()));
+    echo $this->tag('meta', array('property' => 'og:image', 'content' => $this->post->sample_url()));
+}
+?>
